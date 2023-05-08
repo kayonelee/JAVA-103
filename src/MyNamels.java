82eb6946dfe0925652cb0a823f461a58d8841ce2
kayonelee <klee93@live.com>
2023-05-08 14:24:27 -0400

    commit kl

--- src/MyNamels.java
+++ src/MyNamels.java
@@ -1,7 +1,7 @@
 public class MyNamels {
     public static void main(String[] args) {
         System.out.println("My name is");
-        System.out.println("Larry");
-        System.out.println("Sprinkle");
+        System.out.println("Kayone");
+        System.out.println("Lee");
     }
 }
